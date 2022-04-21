cd39b855891468d272a93afbf40966c8ba7fd06a
leo-mutuku <le.mutuku@gmail.com>
2022-04-21 13:56:40 +0300

    JWT Request Filter

---
+++ src/main/java/com/mogul/jwt/configuration/JwtRequestFilter.java
@@ -0,0 +1,4 @@
+package com.mogul.jwt.configuration;
+
+public class JwtRequestFilter {
+}
--- src/main/java/com/mogul/jwt/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/mogul/jwt/configuration/WebSecurityConfiguration.java
@@ -1,24 +1,40 @@
 package com.mogul.jwt.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private JWTEntryAuthenticationPoint jwtEntryAuthenticationPoint;
     @Bean
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception{
         return  super.authenticationManagerBean();
     }
-
+    @Override
     public void configure(HttpSecurity httpSecurity){
-        httpSecurity.cors()
+        httpSecurity.cors();
+        httpSecurity.csrf().disable()
+                .authorizeHttpRequests().antMatchers("").permitAll()
+                .antMatchers(HttpHeaders.ALLOW).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(jwtEntryAuthenticationPoint)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        ;
+        httpSecurity.addFilterBefore()
+
     }
 }
