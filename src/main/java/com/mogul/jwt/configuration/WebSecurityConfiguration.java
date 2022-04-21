0e34c3b8996df7f032eb0f2b4866b88f241e43ec
leo-mutuku <le.mutuku@gmail.com>
2022-04-21 15:22:55 +0300

    UserDetail service class implementation

--- src/main/java/com/mogul/jwt/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/mogul/jwt/configuration/WebSecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.mogul.jwt.configuration;
 
+import com.mogul.jwt.service.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -20,6 +22,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @EnableGlobalMethodSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsService jwtService;
 
     @Autowired
     private JWTEntryAuthenticationPoint jwtEntryAuthenticationPoint;
@@ -51,8 +55,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder){
-        authenticationManagerBuilder.userDetailsService()
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+        authenticationManagerBuilder.userDetailsService(jwtService).passwordEncoder(passwordEncoder());
     }
 
 }
